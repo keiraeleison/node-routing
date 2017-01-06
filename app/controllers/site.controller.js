5257a1c4301f7f99245f3c5c212ba648e455f43b
keiraeleison <keiraeleison@gmail.com>
2017-01-06 13:50:02 +0100

    Bringing in controllers to clean up the structure even more

---
+++ app/controllers/site.controller.js
@@ -0,0 +1,46 @@
+var path = require('path');
+
+module.exports = {
+  showHome: showHome,
+  showAbout: showAbout,
+  showContact: showContact,
+  processContact: processContact,
+  showProfile: showProfile,
+  show404: show404
+};
+
+function showHome(req,res) {
+  //res.send('Welcome to the home page!');
+  res.sendFile(path.join(__dirname, '../../index.html'));
+};
+
+function showAbout(req, res) {
+  res.json({message: 'This will be the about page' });
+}; 
+
+function showContact(req, res) {
+  res.sendFile(path.join(__dirname, '../../contact.html'));
+};
+
+function processContact(req, res) {
+  console.log(req.body);
+  res.send('Hello, ' + req.body.name);
+};
+
+function showProfile(req, res) {
+    // Note: 
+    // console.log(req.params) is displayed as expected, but
+    // console.log("object: " + obj) would be displayed as: "object: [Object obj]"
+    // so instead use console.log("object: %O", obj) OR JSON.stringify(obj)
+  console.log('These are the parameters: ' + JSON.stringify(req.params));
+
+  // grab user profile
+  // grab the post
+  res.send('You are reading ' + req.params.post_slug + ' from ' + req.params.username);
+};
+
+function show404(req, res) {
+  res.status(404);
+  //res.send('404 not found');
+  res.sendFile(path.join(__dirname, '../../404.html'));
+};
--- app/routes.js
+++ app/routes.js
@@ -1,52 +1,25 @@
 var express = require('express');
 var router  = express.Router();
 var path = require('path');
+var  siteController = require('./controllers/site.controller');
+var dashboardController = require('./controllers/dashboard.controller');
 
 // export the router
 module.exports = router;
 
 // routes defined here
 
-// apply routes
-router.get('/',         showHome);
-router.get('/about',    showAbout);
-router.get('/contact',  showContact); 
-router.post('/contact', processContact);
-router.get('/:username/:post_slug', showProfile); 
-router.use(show404);
+// site routes ==================================
+router.get('/',         siteController.showHome);
+router.get('/about',    siteController.showAbout);
+router.get('/contact',  siteController.showContact); 
+router.post('/contact', siteController.processContact);
+router.get('/:username/:post_slug', siteController.showProfile); 
 
-function showHome(req,res) {
-  //res.send('Welcome to the home page!');
-  res.sendFile(path.join(__dirname, '../index.html'));
-};
+// dashboard routes =============================
+router.get('/dashboard', dashboardController.showDashboard);
 
-function showAbout(req, res) {
-  res.json({message: 'This will be the about page' });
-}; 
+// api routes ===================================
 
-function showContact(req, res) {
-  res.sendFile(path.join(__dirname, '../contact.html'));
-};
-
-function processContact(req, res) {
-  console.log(req.body);
-  res.send('Hello, ' + req.body.name);
-};
-
-function showProfile(req, res) {
-    // Note: 
-    // console.log(req.params) is displayed as expected, but
-    // console.log("object: " + obj) would be displayed as: "object: [Object obj]"
-    // so instead use console.log("object: %O", obj) OR JSON.stringify(obj)
-  console.log('These are the parameters: ' + JSON.stringify(req.params));
-
-  // grab user profile
-  // grab the post
-  res.send('You are reading ' + req.params.post_slug + ' from ' + req.params.username);
-};
-
-function show404(req, res) {
-  res.status(404);
-  //res.send('404 not found');
-  res.sendFile(path.join(__dirname, '../404.html'));
-};
+//404
+router.use(siteController.show404);
